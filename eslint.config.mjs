8b0ca4575de57476e772519eb61072263d3d47b6
Kelvinrandu <randukelvin@gmail.com>
2025-01-27 15:08:11 +0300

    update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.rules("@typescript-eslint/no-empty-object-type", "off"),
 ];
 
 export default eslintConfig;
