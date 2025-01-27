98599283e079aba422721405ec24766bd3724fe7
Kelvinrandu <randukelvin@gmail.com>
2025-01-27 16:20:26 +0300

    fix color mode

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ export default function CaptionCarousel() {
     },
     {
       title: 'Beach Tours',
-      text: "Curated Beach Tours  just for yo",
+      text: "Curated Beach Tours  just for you",
       image:
         'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     },
