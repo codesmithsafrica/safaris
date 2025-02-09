90c3aead67d3804639265c43f1e543eb4cc50d8c
Kelvinrandu <randukelvin@gmail.com>
2025-02-10 00:04:46 +0300

    added wordings

--- components/App.tsx
+++ components/App.tsx
@@ -10,6 +10,11 @@ import {
   Image,
   useDisclosure,
   Collapsible,
+
+  chakra,
+  Container,
+
+  VisuallyHidden,
 } from '@chakra-ui/react'
 import {
   PopoverContent,
@@ -26,6 +31,8 @@ import {
 } from "@/components/ui/drawer"
 import { Moon, Sun, Menu, ChevronDown } from "lucide-react"
 import { useColorModeValue, useColorMode } from './ui/color-mode'
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+
 type Iprops = {
   children: ReactNode;
 
@@ -102,7 +109,69 @@ const App = ({ children }: Iprops) => {
         color={useColorModeValue('gray.600', 'white')} >
         {children}
       </Box>
+      <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container
+        as={Stack}
+        maxW={'6xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+         //@ts-expect-error:type fix
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}>
+            <Image h={15}  src={'logo.jpeg'} alt={'logo'} />
+        <Text>© 2025 Maiyo Bespoke Tours and Travel . All rights reserved</Text>
+        <Stack direction={'row'} 
+         //@ts-expect-error:type fix
+        spacing={6}>
+          <SocialButton label={'Twitter'} href={'#'}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={'YouTube'} href={'#'}>
+            <FaYoutube />
+          </SocialButton>
+          <SocialButton label={'Instagram'} href={'#'}>
+            <FaInstagram />
+          </SocialButton>
+        </Stack>
+      </Container>
+    </Box>
     </>
+  )
+}
+
+
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode
+  label: string
+  href: string
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+       //@ts-expect-error:type fix
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
   )
 }
 
