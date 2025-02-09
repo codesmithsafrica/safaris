5f621c3dc80b8dab38848e46e8ee9393d6dca58d
Kelvinrandu <randukelvin@gmail.com>
2025-02-10 01:52:28 +0300

    added contact us

--- app/page.tsx
+++ app/page.tsx
@@ -121,21 +121,21 @@ export default function Home() {
       <Stack
          //@ts-expect-error:fix 
        spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
-        <Heading fontSize={'3xl'}>This is the headline</Heading>
+   
         <Text color={'gray.600'} fontSize={'xl'}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+  We offer the following services
         </Text>
       </Stack>
 
       <Container maxW={'6xl'} mt={10}>
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} 
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 2 }} 
            //@ts-expect-error:fix 
         spacing={10}>
           {features.map((feature) => (
             <HStack key={feature.id} align={'top'}>
               <Box color={'green.400'} px={2}>
                 {/* <Icon as={CheckIcon} /> */}
+                <Icon as={CheckIcon} w={10} h={10} />
               </Box>
               <VStack align={'start'}>
                 <Text fontWeight={600}>{feature.title}</Text>
--- app/safaris/page.tsx
+++ app/safaris/page.tsx
@@ -1,33 +1,24 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-// import { Card } from '@randukelvin/card'
 import App from '@/components/App'
 import {
-  SimpleGrid,
-  Box, Text, Flex, 
+ Text, Flex,Box
 } from "@chakra-ui/react";
 import { useColorModeValue } from '@/components/ui/color-mode'
-import SearchBar from '@/components/SearchBar';
+import { useParams } from 'next/navigation'
 // import axios from "axios";
-import { Card } from "@/components/Card";
-import { ItemProps } from '../page';
-// import {
-//   Skeleton,
-// } from "@/components/ui/skeleton"
+import Spot from '@/components/Spot';
 
 interface Props {
-  items: ItemProps[];
-  key: number;
+  item: {name?:string,description?:string ,img:string,category:{name:string|undefined}};
+  
 }
-export default function Home() {
-  const items={
 
-  }
-  const [search, setSearch] = React.useState("");
-  // const [items, SetItems] = useState<Props | null>(null)
+export default function Home() {
+  const params = useParams<{ id: string }>()
+  const [item, SetItems] = useState<Props | null>(null)
   const [loading, setLoading] = useState(false);
-  // const itemsApiURL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/items`;
-  const color = useColorModeValue('gray.600', 'white')
+  // const itemsApiURL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/item/${params?.id}`;
   // useEffect(() => {
   //   setLoading(true)
   //   // eslint-disable-next-line
@@ -35,124 +26,32 @@ export default function Home() {
   //     SetItems(response?.data);
   //   });
   //   setLoading(false)
-  // }, [items, itemsApiURL, loading]);
+  // }, [item, itemsApiURL]);
   useEffect(()=>{
     setLoading(true)
     setTimeout(() => {
-      console.log("Delayed for 3 second.");
+      console.log("Delayed for 1 second.");
       setLoading(false)
     }, 3000)
  
 
-  },[loading])
-  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
+  },[])
+  console.log(item?.item?.name,loading,SetItems,setLoading)
 
-    const searchValue = e.target.value;
-    const valueWithoutSlash = searchValue.replace("/", "");
-    setSearch(valueWithoutSlash);
-    return valueWithoutSlash;
-  };
-  // // eslint-disable-next-line
-  // const matchesSearch = (item: any) =>
-  //   item.name.toLowerCase().includes(search.toLowerCase())
-  // const filteredItems = items?.items
-  //   .filter(matchesSearch)
-console.log(setLoading)
   return (
     <App>
       <Flex
         bg={useColorModeValue('white', '#0a0a0a')}
         color={useColorModeValue('gray.600', 'white')}
-
-
       >
         <Text ml={2} mb={2} fontSize="sm">
           {"All"}{' '}
-          <b>{"Safaris"}</b>
+          <b>{params?.id}</b>
         </Text>
       </Flex>
-
-      <Flex
-        justifyContent={'center'}
-        alignItems={'center'}
-        bg={useColorModeValue('white', '#0a0a0a')}
-        color={useColorModeValue('gray.600', 'white')}
-
-      >
-
-
-        <SearchBar search={search} onSearch={onSearch} />
-      </Flex>
-
-      <Box bg={useColorModeValue('white', '#0a0a0a')}
-        color={useColorModeValue('gray.600', 'white')}>
-        <SimpleGrid
-          bg={useColorModeValue('white', '#0a0a0a')}
-          color={useColorModeValue('gray.600', 'white')}
-          paddingTop={20} paddingX={['10px', '20%']} columns={[2, null, 4]} gap={["20px", "40px"]}>
-               <Box as={'a'}
-            //@ts-expect-error:typefix
-            href={`item/1`}>
-            <Card />
-          </Box>
-          <Box as={'a'}
-            //@ts-expect-error:typefix
-            href={`item/2`}>
-            <Card />
-          </Box>
-          <Box as={'a'}
-            //@ts-expect-error:typefix
-            href={`item/3`}>
-            <Card />
-          </Box>
-          <Box as={'a'}
-            //@ts-expect-error:typefix
-            href={`item/4`}>
-            <Card />
-          </Box>
-{/* 
-          {loading ? (
-            <Flex pt={24} align="center" justify="center">
-              <VStack colorPalette="teal">
-                <Spinner color={color} />
-                <Text >Loading Spots...</Text>
-              </VStack>
-            </Flex>
-          ) : (
-            <>
-              {filteredItems?.length ? (
-                // eslint-disable-next-line
-                filteredItems?.map((item: any, index: any) => <Box as={'a'}  
-                //@ts-expect-error:typefix
-                href={`item/${item?.id}`}><Card name={item?.name} src={`/restaurants/${item?.image}`}badge={item?.category?.name} /></Box>)
-              ) :     (   <>  <Stack gap="6" maxW="xs">
-
-              <Skeleton height="200px" />
-            </Stack>
-            <Stack gap="6" maxW="xs">
-
-              <Skeleton height="200px" />
-            </Stack>
-            <Stack gap="6" maxW="xs">
-
-              <Skeleton height="200px" />
-            </Stack>
-            <Stack gap="6" maxW="xs">
-
-              <Skeleton height="200px" />
-            </Stack></>)
-              
-             }
-            </>
-          )} */}
-           {/* <EmptySearch /> */}
-        </SimpleGrid>
-        {/* <Flex align="center" mt={4} justify="center" as="i" color="gray.500">
-          {filteredItems?.length ? (<>{`Showing ${filteredItems?.length} out of ${items?.items?.length} spots `}</>) : null}
-        </Flex> */}
-      </Box>
-
+      <Box  bg={useColorModeValue('white', '#0a0a0a')}><Spot loading={loading} name={item?.item?.name} 
+      //@ts-expect-error:fix
+      img={item?.item?.image} description={item?.item?.description} badge={item?.item?.category?.name}/></Box>
     </App>
   );
 }
